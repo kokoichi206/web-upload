1ab8dbd9fe3f921564712b710e12f9027c9352bb
kokoichi <tt.300607@gmail.com>
2021-05-22 22:58:46 +0900

    first commit

---
+++ softwared/src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="counter">
+    <p>{{ name }}</p>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    initCount: Number
+  },
+  data(){
+    return {
+      count: this.initCount
+    }
+  },
+  methods: {
+    increment(){
+      this.count++
+      // 親コンポーネントにイベントを伝える
+      this.$emit("emitUp",{name: this.name, counted: this.count })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.counter{
+  width: 200px;
+  margin: 0 auto;
+}
+</style>
